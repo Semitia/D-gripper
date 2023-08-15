74c22ffdfd58c1847bd3567e45638471a0e0c0f4
Semitia <3272979503@qq.com>
2023-08-15 11:15:07 +0800

    添加了指令接口，还差speed-freq转换，pos

--- Src/Nano_Stepper/Nano_Stepper.ino
+++ Src/Nano_Stepper/Nano_Stepper.ino
@@ -1,8 +1,6 @@
 #include<SCoop.h>
 
 const int A1_pin=3,A2_pin=4,B1_pin=5,B2_pin=6, LED_pin=13;
-const int next[5] = {0,4,3,1,2},
-          last[5] = {0,3,4,2,1};
 
 /**
  * 信息打印线程
@@ -35,6 +33,9 @@ void setup() {
   mySCoop.start();
 }
 
+/**
+ * 单步控制函数
+*/
 void set_step(int ID)
 {
   switch(ID){
@@ -108,10 +109,15 @@ void set_step(int ID)
     return;
 }
 
-void set_motor(int delay_ms, bool dir)
+uint16_t position=0, target_pos=0;
+const short next[5] = {0,4,3,1,2},
+          last[5] = {0,3,4,2,1};
+void set_speed(int delay_ms, bool dir)
 {
   static int tem_step=1;
   
+  //update position
+
   if(dir) {
     tem_step=next[tem_step];
     set_step(tem_step);
@@ -126,26 +132,111 @@ void set_motor(int delay_ms, bool dir)
   return;
 }
 
+void set_position(uint16_t tar_pos)
+{
+  //position guide the speed
+
+  return;
+}
+
 static int freq=10;
-static bool dir=1;
+static bool dir=1, ctrl_mode=1;//0:position 1:speed
 void loop() {
-
-  set_motor(freq,dir);
-
+  if(ctrl_mode) set_speed(freq,dir);
+  else set_position(target_pos);
   yield();
 }
 
+/**
+ * 串口数据处理函数，四种消息类型
+ * @param buf 串口数据
+ * byte 0：类型标识
+ *  0x01设置速度
+ *    byte 1：方向
+ *    byte 2~3：速度大小，现在其实是步进时间间隔
+ *  0x02设置位置
+ *    byte 1~2：坐标
+ *  0x03读取速度
+ *  0x04读取位置
+*/  
+void buf_process(uint8_t *buf)
+{
+  uint8_t type=buf[0];
+  switch(type){
+    case 0x01:
+      dir=buf[1];
+      freq=buf[2]<<8|buf[3];
+      Serial.print("set speed: ");
+      Serial.print(dir);
+      Serial.print(" ");
+      Serial.println(freq);
+      break;
+    case 0x02:
+      target_pos=buf[1]<<8|buf[2];
+      Serial.print("set target position: ");
+      Serial.println(target_pos);
+      break;
+    case 0x03:
+      Serial.println("read speed: ");
+      Serial.print(dir);
+      Serial.print(" ");
+      Serial.println(freq);
+      break;
+    case 0x04:
+      Serial.println("read position: ");
+      Serial.println(position); 
+      break;
+    default:
+      Serial.println("error cmd type");
+      break;
+  }
+}
+
 /*
-serialEvent是一种在接收到串口数据时自动调用的函数，它可以让你在loop()函数执行完后处理串口数据。
-serialEvent会在每次loop()函数结束时被检查，如果串口缓冲区中有可用的数据，就会触发serialEvent。
-你可以使用Serial.read()等函数来读取串口数据，并进行相应的操作。
-如果你的板子有多个串口，你也可以使用serialEvent1()，serialEvent2()等函数来分别处理不同的串口数据。
+  serialEvent是一种在接收到串口数据时自动调用的函数，它可以让你在loop()函数执行完后处理串口数据。
+  serialEvent会在每次loop()函数结束时被检查，如果串口缓冲区中有可用的数据，就会触发serialEvent。
+  你可以使用Serial.read()等函数来读取串口数据，并进行相应的操作。
+  如果你的板子有多个串口，你也可以使用serialEvent1()，serialEvent2()等函数来分别处理不同的串口数据。
 */
 void serialEvent(){
   static int buf_index=0, end_flag=0;
+  static uint8_t buf[10];
   while(Serial.available()>0)
   {
     uint8_t data=Serial.read();
+    buf[buf_index++]=data;
+
+    //结束标识符 0x41 0x49(星野爱呜呜呜)
+    if(end_flag == 0)
+    {
+      if(data == 0x41)
+      {
+        end_flag=1;
+      }
+    }
+    else if(end_flag == 1)
+    {
+      if(data == 0x49)
+      {
+        end_flag=0;
+        buf_index=0;
+        Serial.println("received data");
+        buf_process(buf);
+      }
+      else {
+        end_flag=0;
+        buf_index=0;
+        Serial.println("error data");
+      }
+    }
+
+    if(buf_index > 9)
+    {
+      end_flag=0;
+      buf_index=0;
+      Serial.println("buf overflow");
+    }
 
   }
+
 }
